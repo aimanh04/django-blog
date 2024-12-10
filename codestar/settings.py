42f2efcc18fe7cf4abb2ef8e710323c387b4b8d7
aimanh04 h <aimanhawaemanoh04@gmail.com>
2024-12-10 13:44:16 +0000

    Add views and added django summernote

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-aimanh04-djangoblog-vhk8ozwm0h9.ws-eu117.gitpod.io', ".herokuapp.com"]
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'blog',
 ]
 
