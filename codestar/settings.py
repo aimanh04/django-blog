6f609bf350516de2ee8e5cd674995a7da4f75d1c
aimanh04 h <aimanhawaemanoh04@gmail.com>
2024-12-16 18:52:06 +0000

    Change debug

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-aimanh04-djangoblog-vhk8ozwm0h9.ws-eu117.gitpod.io', ".herokuapp.com"]
 
